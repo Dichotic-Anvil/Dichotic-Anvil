443ac224fbd0ae7bd8490c2c8d763c47071be463
Michael Balarezo <mrezo@Michaels-MacBook-Pro-2.local>
2016-04-07 17:22:32 -0700

    Setup main app module with routeProvider

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,23 @@
-angular.module('borrow', []);
+angular.module('borrow', ['borrow.services','borrow.items','borrow.auth','ngRoute'])
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/signin', {
+      templateUrl: 'app/auth/login.html',
+      controller: 'AuthController'
+    })
+    .when('/signup', {
+      templateUrl: 'app/auth/signup.html',
+      controller: 'AuthController'
+    })
+    .when('/items', {
+      templateUrl: 'app/items/items.html',
+      controller: 'ItemsController',
+    })
+    .otherwise({
+      redirectTo: '/user'
+    });
+    
+    // The $httpInterceptor is added into the array
+    // of interceptors. Think of it like middleware for your ajax calls
+  $httpProvider.interceptors.push('AttachTokens');
+})
