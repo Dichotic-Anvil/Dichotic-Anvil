b98d4b94414052712e54a3c5c25345c4f76f9e41
mrezo <balarezo.m@gmail.com>
2016-06-02 10:22:42 -0700

    Removes spacing and console logs

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,50 +1,40 @@
-//Dashboard controller
-
 angular.module('borrow.dashboard', [])
   .controller('dashController', function($rootScope, $scope, $http, $location, Auth, filepickerService) {
 
-  $scope.items = [];
-  $scope.requestItems = [];
-  $scope.requestMessages = [];
-  $scope.myFriend = '';
-  $scope.borrowing = [];
+    $scope.items = [];
+    $scope.requestItems = [];
+    $scope.requestMessages = [];
+    $scope.myFriend = '';
+    $scope.borrowing = [];
 
-  // Get the user object to populate dashboard
-  $http.get('/api/user/me')
-    .success(function(data){
-      // console.log(JSON.stringify(data));
-      $scope.user = data;
-      console.log('USER!!!!!!!!!!!!!!!',$scope.user)
+    // Get the user object to populate dashboard
+    $http.get('/api/user/me')
+      .success(function(data){
+        $scope.user = data;
 
-      $scope.numPersonalItems = $scope.user.inventory.length;
-      // Iterate over inventory to populate user items
-      for (var i = 0; i < $scope.user.inventory.length; i++) {
-        $scope.items.push($scope.user.inventory[i].picture.url)
-       }
-      // Iterate over inventory to populate items that have requests
-      for (var i = 0; i < $scope.user.inventory.length; i++) {
-        if (!$scope.user.inventory[i].borrowed){
-          if ($scope.user.inventory[i].requests) {
-            // console.log($scope.user.inventory[i].requests);
-            for (var j = 0; j < $scope.user.inventory[i].requests.length; j++) {
-
-              console.log($scope.user.inventory[i].requests[j]);
-              console.log($scope.user.inventory[i].itemName);
-
-              //TODO: Bet able to get requested borrowers name in place of 'Someone';
-              $scope.requestItems.push("Someone would like to borrow the " + $scope.user.inventory[i].itemName);
+        $scope.numPersonalItems = $scope.user.inventory.length;
+        // Iterate over inventory to populate user items
+        for (var i = 0; i < $scope.user.inventory.length; i++) {
+          $scope.items.push($scope.user.inventory[i].picture.url)
+         }
+        // Iterate over inventory to populate items that have requests
+        for (var i = 0; i < $scope.user.inventory.length; i++) {
+          if (!$scope.user.inventory[i].borrowed){
+            if ($scope.user.inventory[i].requests) {
+              for (var j = 0; j < $scope.user.inventory[i].requests.length; j++) {
+                $scope.requestItems.push("Someone would like to borrow the " + $scope.user.inventory[i].itemName);
+              }
             }
           }
         }
-      }
 
-      for (var i = 0; i < $scope.user.borrowing.length; i++) {
-        $scope.borrowing.push($scope.user.borrowing[i].item.itemName)
-      }
-    })
-    .error(function(data) {
-      console.log('Error: ' + data);
-  });
+        for (var i = 0; i < $scope.user.borrowing.length; i++) {
+          $scope.borrowing.push($scope.user.borrowing[i].item.itemName)
+        }
+      })
+      .error(function(data) {
+        console.log('Error: ' + data);
+    });
 
     $scope.viewProfile = function (friendId) {
       $location.path('/profile/'+ friendId);
@@ -78,8 +68,8 @@ angular.module('borrow.dashboard', [])
       })
     };
 
-  $scope.signout = function() {
-    Auth.signout();
-  };
+    $scope.signout = function() {
+      Auth.signout();
+    };
 
 });
